909a78dc00aafe8c5f8a07aeb4611f30a29deb5d
qingdie <xueping@qingdie.net>
2017-04-05 11:48:05 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,28 @@
+﻿var co = require('./index');
+
+var getUser = function (name, cb) {
+    setTimeout(function () {
+        cb('qingdie ' + name);
+    }, 1000);
+};
+var getUser1 = function (name) {
+    return new Promise(function (res) {
+        setTimeout(function () {
+            res('qingdie ' + name);
+        }, 1000);
+    });
+};
+
+
+co(function* (cb) {
+    var user = yield getUser('你好！', cb);
+    console.log(user);
+    var user1 = yield getUser1('棒棒哒！');
+    console.log(user1);
+
+    var ret = yield co(function (cba) {
+        getUser('棒棒哒！', cba('user'));
+        getUser('你好！', cba('user1'));
+    })(2);
+    console.log(ret);
+});
